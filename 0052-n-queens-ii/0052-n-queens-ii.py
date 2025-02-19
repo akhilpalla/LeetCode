6f8457dac84e7683458c37aa756605e088935f4a
Akhil <36575475+akhilpalla@users.noreply.github.com>
2025-02-19 11:55:58 -0500

    Time: 9 ms (66.23%), Space: 18.1 MB (9.96%) - LeetHub

---
+++ 0052-n-queens-ii/0052-n-queens-ii.py
@@ -0,0 +1,13 @@
+class Solution:
+    def totalNQueens(self, n: int) -> int:
+        def dfs(queens, xy_dif, xy_sum):
+            p = len(queens)
+            if p == n:
+                result.append(queens)
+                return
+            for q in range(n):
+                if q not in queens and p-q not in xy_dif and p+q not in xy_sum:
+                    dfs(queens+[q], xy_dif+[p-q], xy_sum+[p+q])
+        result = []
+        dfs([], [], [])
+        return len(result)
