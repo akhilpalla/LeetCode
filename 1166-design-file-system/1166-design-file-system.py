cdaf64c0708a5ab2e59db6f21cd4137df5ae8f23
Akhil <36575475+akhilpalla@users.noreply.github.com>
2025-08-17 12:01:53 -0700

    Time: 52 ms (39.75%), Space: 25.5 MB (18.41%) - LeetHub

---
+++ 1166-design-file-system/1166-design-file-system.py
@@ -0,0 +1,46 @@
+class Tree:
+    def __init__(self, val=None, childredn = defaultdict()):
+        
+        self.children = {}
+        self.val = val
+        
+class FileSystem:
+
+    def __init__(self):
+        self.root = Tree()
+        
+    def createPath(self, path: str, value: int) -> bool:
+        node = self.root
+        if path == "" or path == "/":
+            return False 
+        paths = path.split('/')
+        paths = paths[1:]
+        i = 0
+        while i < len(paths)-1:
+            if paths[i] not in node.children:
+                return False
+            node = node.children[paths[i]]
+            i+=1
+        if paths[i] in node.children: 
+            return False
+        else:
+            node.children[paths[i]] = Tree(value)
+            
+        return True
+
+    def get(self, path: str) -> int:
+        node = self.root
+        paths = path.split('/')
+        paths = paths[1:]
+        for path in paths:
+            if path not in node.children:
+                return -1
+            node = node.children[path]
+        return node.val
+        
+        
+# # Your FileSystem object will be instantiated and called as such:
+# obj = FileSystem()
+# param_1 = obj.createPath("/leet",1)
+# param_1 = obj.createPath("/leet/code",2)
+# param_2 = obj.get(path)
