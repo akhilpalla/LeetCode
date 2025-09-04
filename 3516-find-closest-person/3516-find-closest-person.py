38705d05b3d99ffa70b1e4d2b4b6fdee41f2be09
Akhil <36575475+akhilpalla@users.noreply.github.com>
2025-09-03 22:35:59 -0700

    Time: 0 ms (100%), Space: 18 MB (12.36%) - LeetHub

--- 3516-find-closest-person/3516-find-closest-person.py
+++ 3516-find-closest-person/3516-find-closest-person.py
@@ -1,10 +1,7 @@
 class Solution:
     def findClosest(self, x: int, y: int, z: int) -> int:
-        # Compute absolute distances
         distance1 = abs(x - z)
         distance2 = abs(y - z)
-        
-        # Compare distances
         if distance1 == distance2:
             return 0
         return 1 if distance1 < distance2 else 2
