f2be1b5692cd48b0059e6c25679407cac4eb6814
Akhil <36575475+akhilpalla@users.noreply.github.com>
2025-09-30 21:20:13 -0700

    Time: 0 ms (100%), Space: 17.9 MB (36.78%) - LeetHub

---
+++ 1518-water-bottles/1518-water-bottles.py
@@ -0,0 +1,7 @@
+class Solution:
+    def numWaterBottles(self, numBottles: int, numExchange: int) -> int:
+        ans = numBottles
+        while numBottles >= numExchange :
+            ans = ans + (int)(numBottles / numExchange)
+            numBottles = (int)(numBottles / numExchange) + (numBottles % numExchange)
+        return ans
