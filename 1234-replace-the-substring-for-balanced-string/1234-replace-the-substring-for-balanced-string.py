51ac237210eb231c1f2dc4d78a55f80026a742f3
Akhil <36575475+akhilpalla@users.noreply.github.com>
2025-12-03 19:29:10 -0800

    Time: 242 ms (42.77%), Space: 18 MB (82.72%) - LeetHub

--- 1234-replace-the-substring-for-balanced-string/1234-replace-the-substring-for-balanced-string.py
+++ 1234-replace-the-substring-for-balanced-string/1234-replace-the-substring-for-balanced-string.py
@@ -1,19 +1,19 @@
 class Solution:
+    def is_valid(self,counts, target):
+        return all(v <= target for v in counts.values())
     def balancedString(self, s):
         n = len(s)
         k = n // 4
         count = {'Q': 0, 'W': 0, 'E': 0, 'R': 0}
         for char in s:
             count[char] += 1
-        def is_valid(counts, target):
-            return all(v <= target for v in counts.values())
-        if is_valid(count, k):
+        if self.is_valid(count, k):
             return 0
         min_len = n
         left = 0
         for right in range(n):
             count[s[right]] -= 1
-            while left <= right and is_valid(count, k):
+            while left <= right and self.is_valid(count, k):
                 min_len = min(min_len, right - left + 1)
                 count[s[left]] += 1
                 left += 1
