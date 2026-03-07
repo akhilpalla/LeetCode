77e4e3f59a417071754b82d1094667765e250230
Akhil <36575475+akhilpalla@users.noreply.github.com>
2026-03-07 00:24:19 -0800

    Time: 65 ms (65.53%), Space: 4.1 MB (64.4%) - LeetHub

--- 0194-transpose-file/0194-transpose-file.sh
+++ 0194-transpose-file/0194-transpose-file.sh
@@ -1,2 +1,11 @@
 # Read from the file file.txt and print its transposed content to stdout.
-awk '{for (i=1; i<=NF; i++) a[i]=a[i]?a[i]" "$i:$i} END {for (i in a) print a[i]}' file.txt
+awk '{
+  for (i = 1; i <= NF; i++) {
+    col[i] = col[i] (NR > 1 ? " " : "") $i
+  }
+}
+END {
+  for (i = 1; i <= NF; i++) {
+    print col[i]
+  }
+}' file.txt
