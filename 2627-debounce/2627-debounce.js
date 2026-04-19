2b4195885e434c2313ec3c2e9552296e865a5996
Akhil <36575475+akhilpalla@users.noreply.github.com>
2026-04-18 18:58:49 -0700

    Time: 37 ms (98.01%), Space: 53.2 MB (91.35%) - LeetHub

---
+++ 2627-debounce/2627-debounce.js
@@ -0,0 +1,21 @@
+/**
+ * @param {Function} fn
+ * @param {number} t milliseconds
+ * @return {Function}
+ */
+function debounce(fn, t) {
+    let timeoutId;
+    
+    return function(...args) {
+        clearTimeout(timeoutId);
+        timeoutId = setTimeout(() => fn.apply(this, args), t);
+    };
+}
+
+
+/**
+ * const log = debounce(console.log, 100);
+ * log('Hello'); // cancelled
+ * log('Hello'); // cancelled
+ * log('Hello'); // Logged at t=100ms
+ */
