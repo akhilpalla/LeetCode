47452b93a9ab777f612db8b8a0d450e5650f7969
Akhil <36575475+akhilpalla@users.noreply.github.com>
2026-05-17 01:49:34 -0700

    Time: 11 ms (65.93%), Space: 38.1 MB (26.68%) - LeetHub

---
+++ 1306-jump-game-iii/1306-jump-game-iii.py
@@ -0,0 +1,13 @@
+class Solution:
+    def canReach(self, arr: List[int], start: int) -> bool:
+        n = len(arr)
+        def dfs(i: int) -> bool:
+            if i < 0 or i >= n or arr[i] < 0:
+                return False
+            if arr[i] == 0:
+                return True
+            arr[i] *= -1
+            left = dfs(i + arr[i])
+            right = dfs(i - arr[i])
+            return left or right
+        return dfs(start)
